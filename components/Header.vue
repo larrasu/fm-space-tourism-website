c822a516bbba9d93a31cecce55c0463817fc7f6f
Larra <meixmei0000@gmail.com>
2022-12-09 21:19:33 +0800

    update

--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,31 @@
 <template>
   <header>
     <div class="flex items-center justify-between">
-      <div>
+      <div class="p-5">
         <img src="~/assets/shared/logo.svg" alt="Logo" />
       </div>
-      <div>
+      <div class="sm:hidden p-5">
         <button @click="showMenu = true">
           <img src="~/assets/shared/icon-hamburger.svg" alt="Menu" />
         </button>
       </div>
+      <nav class="sm:block bg-white/5 hidden">
+        <div
+          class="flex gap-10 text-[14px] tracking-[2.36px] uppercase font-condensed px-10"
+        >
+          <NuxtLink
+            :to="link.path"
+            v-for="link of menu"
+            class="py-9 flex items-center gap-2"
+            active-class="border-b border-white"
+          >
+            {{ link.title }}
+          </NuxtLink>
+        </div>
+      </nav>
     </div>
     <div
-      class="backdrop-blur-2xl bg-white/4 h-screen absolute right-0 top-0 z-50 transition-all trans"
+      class="backdrop-blur-2xl bg-white/4 trans absolute top-0 right-0 z-50 h-screen transition-all"
       :class="showMenu ? 'w-3/4' : 'w-0'"
     >
       <div class="p-10">
@@ -21,7 +35,7 @@
 
         <nav class="mt-20">
           <ul
-            class="flex flex-col gap-5 text-[16px] tracking-[2.7px] uppercase"
+            class="flex font-condensed flex-col gap-5 text-[16px] tracking-[2.7px] uppercase"
           >
             <li v-for="(link, index) of menu" class="flex items-center gap-2">
               <p class="font-bold">0{{ index }}</p>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center mt-20">
+  <div class="flex flex-col items-center px-5 mt-20">
     <h5>So, you want to travel to</h5>
     <h2 class="uppercase">Space</h2>
     <p class="text-primary text-center">
@@ -7,16 +7,17 @@
       to outer space and not hover kind of on the edge of it. Well sit back, and
       relax because we’ll give you a truly out of this world experience!
     </p>
-    <div
-      class="group cursor-pointer relative w-[250px] h-[250px] mx-auto flex items-center justify-center mt-20"
+    <NuxtLink
+      to="/destination"
+      class="group cursor-pointer relative w-[250px] h-[250px] sm:w-[342px] sm:h-[342px] mx-auto flex items-center justify-center my-20"
     >
       <div class="explore mx-auto">
-        <NuxtLink to="/explore">Explore</NuxtLink>
+        <p>Explore</p>
       </div>
       <div
-        class="explore-glow group-hover:w-[250px] group-hover:h-[250px] group-hover:top-0 group-hover:left-0"
+        class="explore-glow group-hover:w-[250px] group-hover:h-[250px] sm:group-hover:w-[342px] sm:group-hover:h-[342px] group-hover:top-0 group-hover:left-0"
       ></div>
-    </div>
+    </NuxtLink>
   </div>
 </template>
 
@@ -26,9 +27,17 @@ body {
   background-size: cover;
 }
 
-@media screen and (min-width: 768px) {
+@media screen and (min-width: 640px) {
+  body {
+    background: url("~/assets/home/background-home-tablet.jpg");
+    background-size: cover;
+  }
+}
+
+@media screen and (min-width: 1024px) {
   body {
     background: url("~/assets/home/background-home-desktop.jpg");
+    background-size: cover;
   }
 }
 </style>
