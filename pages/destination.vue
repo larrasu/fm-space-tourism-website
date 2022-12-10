c9eff7aefacf4c181778b22eb22fd8249b3bd547
Mei <74983120+meixsu@users.noreply.github.com>
2022-12-10 12:26:58 +0800

    tablet layout done

--- pages/destination.vue
+++ pages/destination.vue
@@ -4,7 +4,9 @@
   >
     <Header />
     <main>
-      <div class="nav-text mb-10 text-center">
+      <div
+        class="nav-text sm:text-left sm:ml-10 mb-10 text-center sm:text-[20px] sm:tracking-[3.28px] sm:mt-10"
+      >
         <span class="text-white/25 font-bold">01</span> Pick Your Destination
       </div>
       <div>
@@ -12,9 +14,11 @@
           <img
             :src="destination.images.png"
             :alt="`Image of ${destination.name}`"
-            class="mx-auto transition-opacity duration-700"
+            class="mx-auto transition-opacity duration-500"
             :class="
-              selected == index ? 'w-[170px] opacity-100' : 'w-0 opacity-0'
+              selected == index
+                ? 'w-[170px] sm:w-[300px] opacity-100'
+                : 'w-0 opacity-0'
             "
           />
         </div>
@@ -28,23 +32,28 @@
             {{ destination.name }}
           </button>
         </nav>
-        <div>
+        <div class="sm:max-w-xl sm:mx-auto">
           <div v-for="(destination, index) of destinations">
-            <div v-if="selected == index" class="px-5 mt-10 text-center">
+            <div
+              v-if="selected == index"
+              class="sm:mt-8 px-5 mt-10 text-center"
+            >
               <h3>{{ destination.name }}</h3>
-              <div class="border-[#383B4B] mt-2 border-b pb-5">
-                <p class="text-primary text-[15px] leadiing-[25px]">
+              <div class="border-[#383B4B] mt-2 border-b pb-5 sm:pb-12">
+                <p
+                  class="text-primary text-[15px] leadiing-[25px] sm:text-[16px] sm:leading-[28px]"
+                >
                   {{ destination.description }}
                 </p>
               </div>
-              <div class="grid gap-4 mt-5">
+              <div class="sm:flex sm:justify-around grid gap-4 mt-5">
                 <div>
                   <p class="sub-h2 text-primary">Avg. Distance</p>
-                  <p class="sub-h1">{{ destination.distance }}</p>
+                  <p class="sub-h1 mt-3">{{ destination.distance }}</p>
                 </div>
                 <div>
                   <p class="sub-h2 text-primary">Est. Travel Time</p>
-                  <p class="sub-h1">{{ destination.travel }}</p>
+                  <p class="sub-h1 mt-3">{{ destination.travel }}</p>
                 </div>
               </div>
             </div>
