150f701749864e179193b85e6288d5992be42dbc
Mei <74983120+meixsu@users.noreply.github.com>
2022-12-10 12:00:25 +0800

    added technology page, mobile layout done

---
+++ pages/technology.vue
@@ -0,0 +1,65 @@
+<template>
+  <div
+    class="bg-technology-mobile sm:bg-technology-tablet lg:bg-technology-desktop min-h-screen bg-cover"
+  >
+    <Header />
+
+    <main>
+      <div>
+        <div class="nav-text text-center">
+          <span class="text-white/25 font-bold">03</span> Technology
+        </div>
+        <div class="my-10">
+          <div v-for="(technology, index) of technologies">
+            <img
+              :src="technology.images.landscape"
+              :alt="`Image of ${technology.name}`"
+              class="mx-auto transition-opacity duration-500"
+              :class="
+                selected == index ? 'w-full opacity-100' : 'w-0 opacity-0'
+              "
+            />
+          </div>
+        </div>
+        <nav class="flex items-center justify-center gap-4">
+          <button
+            v-for="(technology, index) of technologies"
+            @click="selected = index"
+            class="hover:border-white w-10 h-10 font-serif text-[16px] transition border rounded-full"
+            :class="
+              selected == index
+                ? 'bg-white text-accent'
+                : 'text-white border-white/25'
+            "
+          >
+            {{ index + 1 }}
+          </button>
+        </nav>
+        <div class="mt-10">
+          <div v-for="(technology, index) of technologies">
+            <div v-if="selected == index" class="px-10 text-center">
+              <div
+                class="font-condensed text-primary text-[14px] tracking-[2.36px] uppercase"
+              >
+                The terminology...
+              </div>
+              <div class="font-serif text-[24px] uppercase mb-5">
+                {{ technology.name }}
+              </div>
+              <div class="text-primary text-[15px] leading-[25px]">
+                {{ technology.description }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import data from "~/data.json";
+
+const selected = ref(0);
+const technologies = data.technology;
+</script>
