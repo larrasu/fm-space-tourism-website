f63c6f39ac395bdacbb3a90df70cbbfb9113875f
Mei <74983120+meixsu@users.noreply.github.com>
2022-12-10 12:51:37 +0800

    tablet layout done

--- pages/technology.vue
+++ pages/technology.vue
@@ -6,10 +6,12 @@
 
     <main>
       <div>
-        <div class="nav-text text-center">
-          <span class="text-white/25 font-bold">03</span> Technology
+        <div
+          class="nav-text sm:text-left sm:ml-10 text-center sm:text-[20px] sm:tracking-[3.28px] sm:mt-10"
+        >
+          <span class="text-white/25 font-bold">03</span> Space Launch 101
         </div>
-        <div class="my-10">
+        <div class="sm:my-14 my-10">
           <div v-for="(technology, index) of technologies">
             <img
               :src="technology.images.landscape"
@@ -25,7 +27,7 @@
           <button
             v-for="(technology, index) of technologies"
             @click="selected = index"
-            class="hover:border-white w-10 h-10 font-serif text-[16px] transition border rounded-full"
+            class="hover:border-white w-10 h-10 sm:w-[60px] sm:h-[60px] font-serif text-[16px] sm:text-[24px] transition border rounded-full"
             :class="
               selected == index
                 ? 'bg-white text-accent'
@@ -35,18 +37,22 @@
             {{ index + 1 }}
           </button>
         </nav>
-        <div class="mt-10">
+        <div class="sm:max-w-lg sm:mx-auto mt-10">
           <div v-for="(technology, index) of technologies">
             <div v-if="selected == index" class="px-10 text-center">
               <div
-                class="font-condensed text-primary text-[14px] tracking-[2.36px] uppercase"
+                class="font-condensed text-primary text-[14px] tracking-[2.36px] sm:text-[16px] sm:tracking-[2.7px] uppercase"
               >
                 The terminology...
               </div>
-              <div class="font-serif text-[24px] uppercase mb-5">
+              <div
+                class="font-serif text-[24px] uppercase mb-4 sm:mt-4 sm:text-[40px]"
+              >
                 {{ technology.name }}
               </div>
-              <div class="text-primary text-[15px] leading-[25px]">
+              <div
+                class="text-primary text-[15px] leading-[25px] sm:text-[16px] sm:leading-[28px]"
+              >
                 {{ technology.description }}
               </div>
             </div>
